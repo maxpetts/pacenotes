58e660be5e16cb7745b1a6502e310a952b522334
maxpetts <maxpetts9@gmail.com>
2022-01-01 18:35:20 +0000

    Finish get angle from 3 points function

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ mod point {
             if long < -180.0 || long > 180.0 {
                 panic!("Out of bounds. Longitude must be above -180 and below 180");
             }
-
             if lat < -90.0 || lat > 90.0 {
                 panic!("Out of bounds. Latitude must be above -90 and below 90");
             }
@@ -41,17 +40,17 @@ fn main() {
     let p2 = Coordinate::new(4.0, 2.0);
     let p3 = Coordinate::new(4.0, 7.0);
 
-    println!("angle: {}", find_angle_at_p2(p1, p2, p3));
+    println!("angle: {}", find_angle_at_p2_as_rad(p1, p2, p3));
 }
 
-fn find_angle_at_p2(p1: Coordinate, p2: Coordinate, p3: Coordinate) -> f32 {
+fn find_angle_at_p2_as_rad(p1: Coordinate, p2: Coordinate, p3: Coordinate) -> f32 {
     let dist_p1p2: f32 = ((p1.long - p2.long).powi(2) + (p1.lat - p2.lat).powi(2)).sqrt(); // Dont need to sqrt if going to ^2 later
     let dist_p2p3: f32 = ((p2.long - p3.long).powi(2) + (p2.lat - p3.lat).powi(2)).sqrt();
-    let dist_p3p1: f32 = ((p3.long - p1.long).powi(2) + (p3.lat - p1.lat).powi(2)).sqrt();
-    println!(
-        "dist_p1p2: {}, dist_p2p3: {}, div: {}",
-        dist_p1p2,
-        dist_p2p3,
-        dist_p1p2 / &dist_p2p3
-    );
+    let dist_p3p1: f32 = ((p3.long - p1.long).powi(2) + (p3.lat - p1.lat).powi(2))
+        .sqrt()
+        .abs();
+
+    return (dist_p1p2.powi(2) + dist_p2p3.powi(2)
+        - dist_p3p1.powi(2) / 2.0 * dist_p1p2 * dist_p2p3)
+        .acos();
 }
