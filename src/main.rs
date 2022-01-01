adf32a85d0a0adc100779da87c93fd4aeb51f715
maxpetts <maxpetts9@gmail.com>
2022-01-01 13:28:31 +0000

    Refactor rest of code to fall inline with struct rename

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,17 @@ impl fmt::Display for Coordinate {
         write!(f, "[{}, {}]", self.long, self.lat)
     }
 }
+
+fn main() {
+    let p1 = Coordinate::new(1.0, 2.0);
+    let p2 = Coordinate::new(4.0, 2.0);
+    let p3 = Coordinate::new(4.0, 7.0);
+
+    println!("angle: {}", find_angle_at_p2(p1, p2, p3));
+}
+
+fn find_angle_at_p2(p1: Coordinate, p2: Coordinate, p3: Coordinate) -> f32 {
+    let dist_p1p2: f32 = ((p1.long - p2.long).powi(2) + (p1.lat - p2.lat).powi(2)).sqrt(); // Dont need to sqrt if going to ^2 later
     let dist_p2p3: f32 = ((p2.long - p3.long).powi(2) + (p2.lat - p3.lat).powi(2)).sqrt();
     let dist_p3p1: f32 = ((p3.long - p1.long).powi(2) + (p3.lat - p1.lat).powi(2)).sqrt();
     println!(
