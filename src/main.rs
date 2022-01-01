291e5ec3061db613f20f74d5a2db627dcb0e047d
maxpetts <maxpetts9@gmail.com>
2022-01-01 13:25:43 +0000

    Work on custom struct
    Renamed from LongLat to Coordinate. Implemented display and custom
    constructor.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-fn main() {
-    let p1 = LongLat {
-        long: 1.0,
-        lat: 2.0,
-    };
+use core::fmt;
 
-    let p2 = LongLat {
-        long: 4.0,
-        lat: 2.0,
-    };
-
-    let p3 = LongLat {
-        long: 1.0,
-        lat: 2.0,
-    };
-
-    find_angle_at_p2(p1, p2, p3)
-}
-
-struct LongLat {
-    // Add constructor to keep within bounds
+struct Coordinate {
     long: f32,
     lat: f32,
 }
 
-fn find_angle_at_p2(p1: LongLat, p2: LongLat, p3: LongLat) {
-    let dist_p1p2: f32 = ((p1.long - p2.long).powi(2) + (p1.lat - p2.lat).powi(2)).sqrt();
+impl Coordinate {
+    pub fn new(long: f32, lat: f32) -> Self {
+        if long < -180.0 || long > 180.0 {
+            panic!("Out of bounds. Longitude must be above -180 and below 180");
+        }
+
+        if lat < -90.0 || lat > 90.0 {
+            panic!("Out of bounds. Latitude must be above -90 and below 90");
+        }
+
+        Coordinate {
+            long: long,
+            lat: lat,
+        }
+    }
+}
+
+impl fmt::Display for Coordinate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}, {}]", self.long, self.lat)
+    }
+}
     let dist_p2p3: f32 = ((p2.long - p3.long).powi(2) + (p2.lat - p3.lat).powi(2)).sqrt();
     let dist_p3p1: f32 = ((p3.long - p1.long).powi(2) + (p3.lat - p1.lat).powi(2)).sqrt();
     println!(
